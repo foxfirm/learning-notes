4e22689c0463bef669d108b065057e93f5e3252f
foxfirm <349907447@qq.com>
2024-08-21 01:03:03 +0800

    查看class文件

---
+++ understanding_the_jvm/src/main/java/org/finagm/utj/clazz/TestClass.java
@@ -0,0 +1,15 @@
+package org.finagm.utj.clazz;
+
+/**
+ * TestClass
+ *
+ * @author ligang
+ * @since 2024-08-21
+ */
+public class TestClass {
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
