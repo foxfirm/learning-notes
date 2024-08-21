46047b780461f36ba301e92e81211c2e857f72b0
foxfirm <349907447@qq.com>
2024-08-22 00:02:51 +0800

    魔数查看，十六进制编辑器：HexFiend，查看分析class文件

--- understanding_the_jvm/src/main/java/org/finagm/utj/clazz/TestClass.java
+++ understanding_the_jvm/src/main/java/org/finagm/utj/clazz/TestClass.java
@@ -2,6 +2,10 @@ package org.finagm.utj.clazz;
 
 /**
  * TestClass
+ * <p><a href="https://github.com/HexFiend/HexFiend">HexFiend打开十六进制文件</a>
+ * <p><a href="https://hexfiend.com/">HexFiend官网</a>
+ * <br>class文件魔数：CAFEBABE<br>
+ * <a href="https://gist.github.com/leommoore/f9e57ba2aa4bf197ebc5">magic number参考</a>
  *
  * @author ligang
  * @since 2024-08-21
