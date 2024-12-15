1a05a64bb7012bcf713aadb61836804abdca3af8
foxfirm <349907447@qq.com>
2024-12-15 16:39:15 +0800

    新增leetcode工程

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ learning notes
 # 创建understanding the jvm学习工程
 mvn archetype:generate -DgroupId=org.finagm.utj.quickstart -DartifactId=understanding_the_jvm -DarchetypeArtifactId=maven-archetype-quickstart -Dversion=1.0-snapshot -DarchetypeCatalog=internal
 
-
+# 创建leetcode刷题工程
+mvn archetype:generate -DgroupId=org.finagm.leetcode.quickstart -DartifactId=leetcode_training -DarchetypeArtifactId=maven-archetype-quickstart -Dversion=1.0-snapshot -DarchetypeCatalog=internal
--- understanding_the_jvm/src/main/java/org/finagm/utj/quickstart/App.java
+++ understanding_the_jvm/src/main/java/org/finagm/utj/quickstart/App.java
@@ -1,13 +1,32 @@
 package org.finagm.utj.quickstart;
 
+import org.finagm.utj.clazz.TestClass;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private int y = 0;
+
+    public static void main(String[] args) {
+        App app = new App();
+        int a = 5;
+        app.getClass();
+        char[] c = new char[1];
+        Class dd = char[].class;
+        Class cc = c.getClass();
+        System.out.println(cc);
+        if(c instanceof Object){
+            System.out.println(cc);
+        }
+        Class d = cc.getClass();
+        Class e = d.getClass();
+        Class i = int.class;
+        System.out.println(i);
+
+        System.out.println(Integer.TYPE == int.class);
+
     }
+
+
 }
