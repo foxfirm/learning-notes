39ce994ffb96126ce6d4ef1e5a7664df0668cab4
foxfirm <349907447@qq.com>
2025-06-26 00:34:23 +0800

    编辑距离

--- leetcode_training/src/main/java/org/finagm/leetcode/dp/DPTest.java
+++ leetcode_training/src/main/java/org/finagm/leetcode/dp/DPTest.java
@@ -85,7 +85,7 @@ public class DPTest {
         return dp[m][n];
     }
 
-    public int minDistance(String word1, String word2) {
+    public int minDistance1(String word1, String word2) {
         int m = word1.length(), n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i <= m; i++) {
@@ -121,6 +121,42 @@ public class DPTest {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = Math.min(dp[i - 1][j] + s1.charAt(i - 1), dp[i][j - 1] + s2.charAt(j - 1));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        }
+        int res = dp[0];
+        for (int i = 1; i < n; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + 1);
                 }
             }
         }
